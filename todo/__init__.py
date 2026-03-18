23f4101ac3f8389f868354c10f7e0506bbbb3c25
emlyn-d <bojudeng1102@gmail.com>
2026-03-19 00:43:59 +1000

    Fix health endpoint (no DB dependency)

--- todo/__init__.py
+++ todo/__init__.py
@@ -6,6 +6,12 @@ def create_app(config_overrides=None):
    app = Flask(__name__) 
  
    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
+   
+   @app.route('/health')
+   def root_health():
+       """Return a status of 'ok' if the server is running and listening to request"""
+       return {"status": "ok"}
+   
    if config_overrides: 
        app.config.update(config_overrides)
  
